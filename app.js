7da25aba9c6ec63f765c1c9df191b2ac8b126dd1
GuillaumeDupays <guillaumedupays@gmail.com>
2020-10-14 12:38:51 +0200

    instance de mongoose ajoutée

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
+
 const app = express();
 const api = require('./api/v1/index');
 const cors = require('cors');
+
+const mongoose = require('mongoose');
+const connection = mongoose.connection;
 
 app.set('port', (process.env.port || 3000));
 
 app.use(cors());
 app.use('/api/v1', api); //localhost:3000/api/v1
 
-app.listen(app.get('port'), () => {
-    console.log(`Ecoute le port ${app.get('port')}`);
+app.use((req, res) => {
+    const err = new Error('404 - Non trouvé...');
+    err.status = 500;
+    res.json(err);
+});
+
+mongoose.connect('mongodb://localhost:27017/contacts', { useUnifiedTopology: true, useNewUrlParser: true });
+
+connection.on('error', () => {
+    console.error(`Erreur de connexion vers MongoDb, error: ${err.message}`);
+});
+
+connection.once('open', () => {
+    console.log('Connecté à MongoDb');
+        
+    app.listen(app.get('port'), () => {
+        console.log(`Ecoute le port ${app.get('port')}`);
+        });
 });
