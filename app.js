71b7675ace2be9ce39690d6818f21d27893a5eaa
GuillaumeDupays <guillaumedupays@gmail.com>
2020-11-05 08:45:51 +0100

    prod cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('port', (process.env.port || 3000 ));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors({credentials: true, origin:  'http://blob-zone.com/admin' }));
+app.use(cors({credentials: true, origin: `${prod}`}));
 
 const uploadsDir = require('path').join(__dirname, '/uploads');
 console.log('uploadsDir', uploadsDir);
